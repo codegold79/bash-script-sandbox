4a637c188484d87179eda6e4a9452a0bb02778c6
F. Gold <fgold@vmware.com>
2021-06-17 16:59:19 -0700

    Move call-a-closure.sh to own directory
    Signed-off-by: F. Gold <fgold@vmware.com>

--- closure/call-a-closure.sh
+++ closure/call-a-closure.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-   
+
 # source imports the closure function in array-reader-closure.sh file.
-source helper-functions/array-reader-closure.sh
+source array-reader-closure.sh
 
 # call the function
 readArray
